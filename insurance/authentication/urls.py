ceea7677b8ed32681113bdd9de1675664e12c9a1
Antonio Mourão <antonio.sobrinho@caffeex.com>
2021-02-20 22:43:08 -0300

    change acess token to JWT

--- insurance/authentication/urls.py
+++ insurance/authentication/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from rest_framework.authtoken import views as authtoken_views
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
-    path('token/', authtoken_views.obtain_auth_token)
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
